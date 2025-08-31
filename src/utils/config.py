8c3984fa1b0c94af95ce21d6def5526b2166d4fb
EliAbdiel <371.abdiel@gmail.com>
2025-08-31 00:52:34 -0500

    feat: add memory database environment variable

--- src/utils/config.py
+++ src/utils/config.py
@@ -9,6 +9,7 @@ load_dotenv(override=True)
 GEMINI_KEY = os.environ["GEMINI_API_KEY"]
 GEMINI_KEY_V2 = os.environ["GEMINI_API_KEY_V2"]
 GEMINI_MODEL = os.environ["GEMINI_MODEL"]
+GEMINI_VEO = os.environ["GEMINI_VEO"]
 GEMINI_IMAGE_MODEL = os.environ["GEMINI_IMAGE_GENERATION"]
 GEMINI_2_5_MODEL = os.environ["GEMINI_2_5_MODEL"]
 OPENROUTER_KEY = os.environ["OPENROUTER_API_KEY"]
@@ -16,6 +17,7 @@ OPENROUTER_MODEL = os.environ["OPENROUTER_MODEL_NAME"]
 OPENROUTER_URL = os.environ["OPENROUTER_BASE_URL"]
 
 DATABASE_URL = os.environ["DATABASE_LOCAL_URL"]
+MEMORY_DATABASE = os.environ["MEMORY_DATABASE"]
 
 AZURE_CONTAINER_NAME = os.environ["CONTAINER_NAME"]
 AZURE_STORAGE_ACCOUNT_NAME = os.environ["STORAGE_ACCOUNT_NAME"]
@@ -30,6 +32,7 @@ DATA_DIR = BASE_DIR / 'data'
 
 EXTRACTED_DATA = DATA_DIR / 'extracted_data'
 GENERATED_IMAGES = DATA_DIR / 'generated_images'
+GENERATED_VIDEOS = DATA_DIR / 'generated_videos'
 
 # Usage in services:
 # from utils.config import GENERATED_IMAGES
